b517cd352c9c47d47c42e32fd689cb75f25d179f
Nathaniel D Alcedo Jr <nathanielalcedojr@gmail.com>
2017-02-27 16:02:58 +0800

    Added react-router history wrapper around app and deleted app.test.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router, browserHistory } from "react-router";
 
-import App from "./App";
+import routes from "./routes";
 
 ReactDOM.render(
-  <App />,
-  document.getElementById("root")
+    <Router history={browserHistory} routes={routes} />,
+    document.getElementById("root")
 );
