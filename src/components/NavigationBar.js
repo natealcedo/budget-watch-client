9abb5d143df9f2d84776a7782eaee87609b86388
Nathaniel D Alcedo Jr <nathanielalcedojr@gmail.com>
2017-02-27 20:22:32 +0800

    change this file indent to 2 spaces

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -3,15 +3,15 @@ import { Menu } from "semantic-ui-react";
 import { Link } from "react-router";
 
 const NavigationBar = ({ path }) => (
-    <Menu>
-        <Menu.Menu>
-            <Menu.Item as={Link} to="/">Budget Watch</Menu.Item>
-        </Menu.Menu> 
-        <Menu.Menu position="right">
-            <Menu.Item active={path==="/login"} as={Link} to="/login">Login</Menu.Item>
-            <Menu.Item active={path==="/signup"} as={Link} to="/Signup">Signup</Menu.Item>
-        </Menu.Menu>
-    </Menu> 
+  <Menu inverted  size="huge" color="blue">
+    <Menu.Header>
+      <Menu.Item as={Link} to="/">Budget Watch</Menu.Item>
+    </Menu.Header> 
+    <Menu.Menu position="right">
+      <Menu.Item active={path==="/login"} as={Link} to="/login">Login</Menu.Item>
+      <Menu.Item active={path==="/signup"} as={Link} to="/signup">Signup</Menu.Item>
+    </Menu.Menu>
+  </Menu> 
 );
 
 export default NavigationBar;
