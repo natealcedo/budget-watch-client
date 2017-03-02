0894136f043540b8817de3fc524e692ad487c2b6
Nathaniel D Alcedo Jr <nathanielalcedojr@gmail.com>
2017-03-02 17:16:58 +0800

    Add a welcome page/component

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,14 @@ import { Route, IndexRoute } from "react-router";
 
 import App from "./components/App";
 import Login from "./components/Login/Login";
+import Welcome from "./components/Welcome/Welcome";
 import PageNotFound from "./components/PageNotFound";
 import Signup from "./components/Signup/Signup";
 
 export default(
     <Route>
         <Route path="/" component={App}>
-            <IndexRoute />
+            <IndexRoute component={Welcome} />
             <Route path="login" component={Login} />
             <Route path="signup" component={Signup} />
         </Route>
