ab1c77ab7c195061c33949413cea6edd82cd9171
Nathaniel D Alcedo Jr <nathanielalcedojr@gmail.com>
2017-03-02 14:59:51 +0800

    Add button and loading animation

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,15 +1,20 @@
 import React from "react";
-import { Message, Grid, Form } from "semantic-ui-react";
+import { Message, Button, Grid, Form } from "semantic-ui-react";
 
 import LoginFormInputGroup from "./LoginFormInputGroup";
-const style={
+
+const style = {
   display: "flex",
   flexDirection: "column",
   justifyContent: "center",
   height: "60vh"
 };
 
-const LoginForm = ({ errors, updateFieldState, validateUserInput}) => (
+const buttonStyle = {
+  marginTop: 20
+};
+
+const LoginForm = ({ errors, userLogin, updateFieldState, isLoading, validateUserInput}) => (
   <div style={style}>
     <Grid centered>
       <Grid.Column width={10}>
@@ -36,6 +41,13 @@ const LoginForm = ({ errors, updateFieldState, validateUserInput}) => (
             updateFieldState={updateFieldState}
           />
         </Form>
+        <Button
+          basic style={buttonStyle}
+          loading={isLoading}
+          onClick={userLogin}
+          size="large"
+          primary>Login
+        </Button>
       </Grid.Column>
     </Grid>
   </div>
@@ -44,7 +56,9 @@ const LoginForm = ({ errors, updateFieldState, validateUserInput}) => (
 LoginForm.propTypes = {
   errors: React.PropTypes.object,
   updateFieldState: React.PropTypes.func.isRequired,
-  validateUserInput: React.PropTypes.func.isRequired
+  validateUserInput: React.PropTypes.func.isRequired,
+  userLogin: React.PropTypes.func.isRequired,
+  isLoading: React.PropTypes.bool.isRequired
 };
 
 export default LoginForm;
