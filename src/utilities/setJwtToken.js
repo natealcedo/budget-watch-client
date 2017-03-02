08127e4e1ce794d0e5bdb6feeb87fd838b7bd4f4
Nathaniel D Alcedo Jr <nathanielalcedojr@gmail.com>
2017-03-02 16:04:19 +0800

    fixed type where defaults where not being set

--- src/utilities/setJwtToken.js
+++ src/utilities/setJwtToken.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export default function setJwtToken(token){
   if(token){
-    axios.default.headers.common["authorization"] = `Bearer ${token}`;
+    axios.defaults.headers.common["authorization"] = `Bearer ${token}`;
   } else {
-    delete axios.default.headers.common["authorization"];
+    delete axios.defaults.headers.common["authorization"];
   }
 }
