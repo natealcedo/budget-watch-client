2dea20ee1061e4a6f9c3968497934f8cdf7964ce
Nathaniel D Alcedo Jr <nathanielalcedojr@gmail.com>
2017-03-02 17:16:21 +0800

    rename file to reflect what it does

--- src/utilities/setJwtToken.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default function setJwtToken(token){
-  if(token){
-    axios.defaults.headers.common["authorization"] = `Bearer ${token}`;
-  } else {
-    delete axios.defaults.headers.common["authorization"];
-  }
-}
