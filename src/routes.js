cdeb66155e13557d4f458e14bcd5c2b6e18bd608
Nathaniel D Alcedo Jr <nathanielalcedojr@gmail.com>
2017-03-06 22:56:34 +0800

    fix indentation

--- src/routes.js
+++ src/routes.js
@@ -11,14 +11,14 @@ import Signup from "./components/Signup/Signup";
 import AuthenticateRoute from "./utilities/AuthenticateRoute";
 
 export default(
-    <Route>
-        <Route path="/" component={App}>
-            <IndexRoute component={AuthenticateRoute(Welcome)} />
-            <Route path="login" component={Login} />
-            <Route path="signup" component={Signup} />
-            <Route path="entries" component={AuthenticateRoute(Entry)} />
-            <Route path="entries/addEntry" component={AuthenticateRoute(AddEntry)} />
-        </Route>
-        <Route path="*" component={PageNotFound} />
+  <Route>
+    <Route path="/" component={App}>
+      <IndexRoute component={AuthenticateRoute(Welcome)} />
+      <Route path="login" component={Login} />
+      <Route path="signup" component={Signup} />
+      <Route path="entries" component={AuthenticateRoute(Entry)} />
+      <Route path="entries/addEntry" component={AuthenticateRoute(AddEntry)} />
     </Route>
+    <Route path="*" component={PageNotFound} />
+  </Route>
 );
