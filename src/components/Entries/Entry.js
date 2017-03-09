8343117ad262d8cbf65a1331ab26764ee60859e9
Nathaniel D Alcedo Jr <nathanielalcedojr@gmail.com>
2017-03-09 20:42:47 +0800

    Implement basic view to show entries of the user

--- src/components/Entries/Entry.js
+++ src/components/Entries/Entry.js
@@ -1,14 +1,81 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Container, Form, Table, Dropdown } from "semantic-ui-react";
+import { yearOptions } from "./EntryOptions";
+import { getAllEntries, unsetEntries } from "../../actions/entryActions";
+import EntryRow from "./EntryRow";
 
 class Entry extends React.Component {
+
+  componentWillMount(){
+    this.props.getAllEntries();
+  }
+
+  componentWillUnmount(){
+    this.props.unsetEntries();
+  }
+
   render() {
+    const entryList = this.props.entries.sort((prev, curr) => {
+      return curr.year - prev.year;
+    }).map(entry => (       
+      <EntryRow
+        category={entry.category} 
+        amount={entry.amount}
+        day={entry.day}
+        description={entry.description}
+        month={entry.month}
+        year={entry.year}
+        id={entry._id}
+        key={entry._id}
+      />
+    ));
     return (
-      <div>
-        <h1>View Entries by the month</h1>
-      </div>
+      <Container>
+        <Table >
+          <Table.Header>
+            <Table.Row>
+              <Table.Cell>
+                <Form.Field
+                  control={Dropdown}
+                  fluid
+                  options={yearOptions}
+                  selection
+                  label="View Entries by Time Periods"
+                />
+              </Table.Cell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Category</Table.HeaderCell>
+              <Table.HeaderCell>Description</Table.HeaderCell>
+              <Table.HeaderCell>Day</Table.HeaderCell>
+              <Table.HeaderCell>Month</Table.HeaderCell>
+              <Table.HeaderCell>Year</Table.HeaderCell>
+              <Table.HeaderCell>Amount $</Table.HeaderCell>
+              <Table.HeaderCell>Delete Entry </Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            { entryList } 
+          </Table.Body>
+        </Table>
+      </Container>
     );
   }
 }
 
-export default connect()(Entry);
+function mapStateToProps(state){
+  return {
+    entries: state.entries
+  };
+}
+
+Entry.propTypes = {
+  entries: React.PropTypes.array.isRequired,
+  getAllEntries: React.PropTypes.func.isRequired,
+  unsetEntries: React.PropTypes.func.isRequired
+};
+
+export default connect(mapStateToProps, { getAllEntries, unsetEntries })(Entry);
---
+++ src/components/Entries/EntryRow.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Table, Button } from "semantic-ui-react";
+
+class EntryRow extends React.Component {
+  render() {
+    const { category, description, day, month, year, amount } = this.props;
+    return (
+      <Table.Row>
+        <Table.Cell>{ category }</Table.Cell>
+        <Table.Cell>{ description }</Table.Cell>
+        <Table.Cell>{ day }</Table.Cell>
+        <Table.Cell>{ month }</Table.Cell>
+        <Table.Cell>{ year }</Table.Cell>
+        <Table.Cell>{ amount }</Table.Cell>
+        <Table.Cell><Button negative size="medium">Delete</Button></Table.Cell>
+      </Table.Row>
+    );
+  }
+}
+
+EntryRow.propTypes = {
+  id: React.PropTypes.string.isRequired,
+  amount: React.PropTypes.number.isRequired,
+  day: React.PropTypes.number.isRequired,
+  description: React.PropTypes.string.isRequired,
+  month: React.PropTypes.number.isRequired,
+  year: React.PropTypes.number.isRequired,
+  category: React.PropTypes.string.isRequired,
+  deleteEntry: React.PropTypes.func.isRequired
+};
+
+export default EntryRow;
